98800f7bdc721431096ddf369cb3e53670d123e7
Jonathan Araldi <jonathan.araldi@outlook.com>
2024-04-12 19:27:36 +0200

    Ajout de l'élément composant le `scroll-top` pour remonter en haut de page en fonction du défilement de la page

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,11 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 /*===== SHOW SCROLL TOP =====*/ 
-
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top')
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
 
 /*===== MIXITUP FILTER PORTFOLIO =====*/ 
 
