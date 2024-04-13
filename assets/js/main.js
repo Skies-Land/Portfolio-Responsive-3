a9ad048872973d0038be145d41bf73d938ca8367
Jonathan Araldi <jonathan.araldi@outlook.com>
2024-04-13 18:01:30 +0200

    Ajout des éléments composant la section `portfolio`

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,10 +54,26 @@ function scrollTop(){
 }
 window.addEventListener('scroll', scrollTop)
 
-/*===== MIXITUP FILTER PORTFOLIO =====*/ 
+/*===== MIXITUP FILTER PORTFOLIO =====*/
+const mixer = mixitup('.portfolio__container', {
+    selectors: {
+        target: '.portfolio__content'
+    },
+    animation: {
+        duration: 400
+    }
+});
 
 /* Link active portfolio */ 
+const linkPortfolio = document.querySelectorAll('.portfolio__item')
 
+function activePortfolio(){
+    if(linkPortfolio){
+        linkPortfolio.forEach(l => l.classList.remove('active-portfolio'))
+        this.classList.add('active-portfolio')
+    }
+}
+linkPortfolio.forEach(l => l.addEventListener('click', activePortfolio))
 
 /*===== SWIPER CAROUSEL =====*/ 
 
