7bbcb728ff918bbe3df7249b154f5acf225234c8
Jonathan Araldi <jonathan.araldi@outlook.com>
2024-04-13 20:58:22 +0200

    Ajout des media querie et de l'animation pour la section `home`

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,5 +97,10 @@ const swiper = new Swiper('.testimonial__container', {
 });
 
 /*===== GSAP ANIMATION =====*/ 
+gsap.from('.home__img', {opacity: 0, duration: 2, delay: .5, x:60})
+gsap.from('.home__data', {opacity: 0, duration: 2, delay: .8, y:25})
+gsap.from('.home__greeting, .home__name, .home__profession', {opacity: 0, duration: 2, delay: 1, y: 25, ease:'expo.out', stagger:.2})
 
-
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 2, delay: 1.5, y: 25, ease:'expo.out', stagger:.2})
+gsap.from('.nav__item', {opacity: 0, duration: 2, delay: 1.8, y: 25, ease:'expo.out', stagger:.2})
+gsap.from('.home__social-icon', {opacity: 0, duration: 2, delay: 2.3, y: 25, ease:'expo.out', stagger:.2})
